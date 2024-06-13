d0fc1348206077a0cbe9dea45b61e64f709424a2
David Ortega <davidortegacadena@gmail.com>
2024-06-13 18:06:37 -0500

    full

--- src/main/java/net/unir/ms_inventory_orders/adapter/restful/v1/rest/resttemplate/ProductRestTemplate.java
+++ src/main/java/net/unir/ms_inventory_orders/adapter/restful/v1/rest/resttemplate/ProductRestTemplate.java
@@ -2,20 +2,17 @@ package net.unir.ms_inventory_orders.adapter.restful.v1.rest.resttemplate;
 
 import net.unir.ms_inventory_orders.adapter.restful.v1.models.ProductsAdapterDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.net.http.HttpHeaders;
 
 @RestController
 public class ProductRestTemplate {
     @Autowired
     private RestTemplate restTemplate;
 
-    public ProductsAdapterDTO getProductById(Long id){
-        ResponseEntity<ProductsAdapterDTO> product = restTemplate.getForEntity("http://localhost:8087/products/"+id, ProductsAdapterDTO.class);
+    public ProductsAdapterDTO getProductById(Long id) {
+        ResponseEntity<ProductsAdapterDTO> product = restTemplate.getForEntity("http://localhost:8087/api/v1/products/" + id, ProductsAdapterDTO.class);
         return product.getBody();
     }
 }
